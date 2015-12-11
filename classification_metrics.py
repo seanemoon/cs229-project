7b631feb2f3671d0f81931f5252205f88ded1ede
= <seanemoon@gmail.com>
2015-12-10 23:28:33 -0800

    Adding classification metrics

---
+++ classification_metrics.py
@@ -0,0 +1,79 @@
+import argparse
+import cv2
+
+from classify.classifier import CamClassifier
+
+K_LABELS = ['animal', 'human', 'noise', 'vehicle']
+K_LABEL_INDEX = {label:i for i, label in enumerate(K_LABELS)}
+
+def retrieve_arguments():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("dataset", help="Text file containing list of patches")
+  args = parser.parse_args()
+  return args
+
+def filter_rankings(rankings):
+  labels_of_interest = set(('human', 'noise', 'vehicle'))
+  prob_of_label = {}
+  for label, idx, prob in rankings:
+    if label in labels_of_interest:
+      prob_of_label[label] = prob
+  return prob_of_label
+
+def classify(classifier, patch):
+  rankings = classifier.rankings(patch)
+  prob_of_label = filter_rankings(rankings)
+  if prob_of_label['noise'] < 1.0 - 0.95:
+    if prob_of_label['human'] > 0.6*prob_of_label['vehicle']:
+      return 'human'
+    else:
+      return 'vehicle'
+  else:
+    return 'noise'
+
+def main(args):
+  classifier = CamClassifier()
+  classifier.set_batch_size(1)  # TODO: Batch if its running to slowly.
+
+  with open(args.dataset) as dataset:
+    labeled_patches = [line.split() for line in dataset]
+    labeled_patches = [(x[0], int(x[1])) for x in labeled_patches]
+
+  num_labels = {label: 0 for label in K_LABELS}
+  num_classified = {label: 0 for label in K_LABELS}
+  num_correct = {label: 0 for label in K_LABELS}
+
+  counter = 0
+  for patch, label in labeled_patches:
+    print patch
+    counter += 1
+    # print counter
+    patch = cv2.imread(patch, cv2.IMREAD_COLOR)
+    label = K_LABELS[label]
+    if patch is None:
+      continue
+    classification = classify(classifier, patch)
+
+    print label, classification
+    num_labels[label] += 1
+    num_classified[classification] += 1
+    if label == classification:
+      num_correct[classification] += 1
+
+
+  print num_correct
+  print num_classified
+  print num_labels
+  for label in K_LABELS:
+    num_classified[label] = max(1, num_classified[label])
+    num_labels[label] = max(1, num_labels[label])
+
+  precision = {l: num_correct[l] / float(num_classified[l]) for l in K_LABELS}
+  recall = {l: num_correct[l] / float(num_labels[l]) for l in K_LABELS}
+
+  for label in K_LABELS:
+    print ("%s\n\tprecision: %f\n\trecall: %f" % (label, precision[label], recall[label]))
+
+if __name__ == "__main__":
+  args = retrieve_arguments()
+  main(args)
